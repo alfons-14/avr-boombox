c821ddde62b2643366b60dcc50bbfc74f05af038
alfons-14 <shamden1998@gmail.com>
2016-07-11 10:21:47 +0300

    Add files via upload

---
+++ TWI.cpp
@@ -0,0 +1,216 @@
+/*
+ * TWI.cpp
+ *
+ *  Created on: 16 трав. 2016
+ *      Author: alfons-14
+ */
+#include "TWI.h"
+#include <avr/interrupt.h>
+
+#if TWI_UART_DEBUG==1
+#include "UART.h"
+#include <stdio.h>
+#endif
+
+volatile uint8_t TWI_ADRESS;
+volatile union TWI_state TWI = {0};
+volatile uint8_t *TWI_DATA;
+volatile uint8_t TWI_BYTE;
+volatile uint8_t TWI_DATA_COUNT;
+volatile uint8_t TWI_DATA_POS;
+
+#if TWI_UART_DEBUG==1
+char buf[20];
+#endif
+
+void TWI_init(){
+	TWI.busy=0;
+	TWBR = ((F_CPU / TWI_FREQ) - 16) / (2 * 1);
+	TWI_ENABLE;
+}
+
+void TWI_send(uint8_t adress, uint8_t *data, uint8_t data_count){
+	while(TWI.busy);
+	uint8_t sreg;
+	sreg = SREG;
+	TWI_ADRESS = (adress << 1) | TWI_WRITE;
+	TWI_DATA = data;
+	TWI_DATA_COUNT = data_count;
+//	char buf[20];
+//	sprintf(buf, "\r\nSend to %X adr %d, val %d\r\n", TWI_ADRESS, TWI_DATA[0], TWI_DATA[1]);
+//	UART_send(buf);
+	TWI.busy = 1;
+	TWI_START;
+	SREG = sreg;
+}
+
+void TWI_send_byte(uint8_t adress, uint8_t byte){
+	while(TWI.busy);
+	uint8_t sreg;
+	sreg = SREG;
+	cli();
+	TWI_ADRESS = (adress << 1) | TWI_WRITE;
+	TWI_BYTE = byte;
+	TWI_DATA_COUNT = 1;
+	TWI.busy = 1;
+	TWI_START;
+	SREG = sreg;
+}
+
+void TWI_receive(uint8_t adress, uint8_t *data, uint8_t data_count){
+	while(TWI.busy);
+	uint8_t sreg;
+	sreg = SREG;
+	cli();
+	TWI_ADRESS = (adress << 1) | TWI_READ;
+	TWI_DATA = data;
+	TWI_DATA_COUNT = data_count;
+	TWI.busy = 1;
+	TWI_START;
+	SREG = sreg;
+}
+
+void TWI_repeat(){
+	while(TWI.busy);
+	uint8_t sreg;
+	sreg = SREG;
+	cli();
+	TWI.busy = 1;
+	TWI_START;
+	SREG = sreg;
+
+}
+
+ISR(TWI_vect){
+    if(TWI_ADRESS & TWI_READ){
+    	//read
+    	switch(TWSR){
+
+    	case TWI_START_:
+    	case TWI_RESTART_:
+#if TWI_UART_DEBUG==1
+    		sprintf(buf, "Recieve from 0x%X ", TWI_ADRESS>>1);
+    		UART_send(buf);
+#endif
+    		TWI_DATA_POS=0;
+    		TWDR = TWI_ADRESS;
+    		TWI_SEND;
+    		break;
+
+    	case TWI_SLA_R_ACK_:
+    		if (TWI_DATA_COUNT == 1){
+    			TWI_RECIEVE_LAST;
+    		}
+    		else{
+    			TWI_RECIEVE;
+    		}
+    		break;
+
+    	case TWI_SLA_R_NACK_:
+#if TWI_UART_DEBUG==1
+    		sprintf(buf, "failed: SLA+R+NACK\r\n");
+    		UART_send(buf);
+#endif
+    		TWI.errorCode = TWSR>>2;
+    		TWI.lastOpOK = 0;
+    		TWI.busy = 0;
+    		TWI_STOP;
+    		break;
+
+    	case TWI_RECIEVE_BYTE_:
+    		TWI_DATA[TWI_DATA_POS] = TWDR;
+#if TWI_UART_DEBUG==1
+    		sprintf(buf, "%d ", TWDR);
+    		UART_send(buf);
+#endif
+    		if (++TWI_DATA_POS+1 < TWI_DATA_COUNT){
+    			TWI_RECIEVE;
+    		}
+    		else{
+    			TWI_RECIEVE_LAST;
+    		}
+    		break;
+
+    	case TWI_RECIEVE_BYTE_NACK_:
+#if TWI_UART_DEBUG==1
+    		sprintf(buf, "%d stopped", TWDR);
+    		UART_send(buf);
+#endif
+    		TWI_DATA[TWI_DATA_POS] = TWDR;
+    		TWI_STOP;
+    		TWI.lastOpOK = 1;
+    		TWI.busy = 0;
+    		break;
+    	}
+    }
+    else{
+    	//write
+    	switch(TWSR){
+
+    	case TWI_START_:
+    	case TWI_RESTART_:
+#if TWI_UART_DEBUG==1
+    		sprintf(buf, "Send to 0x%X ", TWI_ADRESS>>1);
+    		UART_send(buf);
+#endif
+    		TWI_DATA_POS=0;
+    		TWDR = TWI_ADRESS;
+    		TWI_SEND;
+    		break;
+
+    	case TWI_SLA_W_ACK_:
+    		if (TWI_DATA_COUNT==1) TWDR = TWI_BYTE; else
+    		TWDR = TWI_DATA[0];
+#if TWI_UART_DEBUG==1
+    		sprintf(buf, "%d ", TWDR);
+    		UART_send(buf);
+#endif
+    		TWI_SEND;
+    		break;
+
+    	case TWI_SLA_W_NACK_:
+#if TWI_UART_DEBUG==1
+    		sprintf(buf, "failed: SLA+W+NACK\r\n");
+    		UART_send(buf);
+#endif
+    		TWI.errorCode = TWSR>>2;
+    		TWI.lastOpOK = 0;
+    		TWI.busy = 0;
+    		TWI_STOP;
+    		break;
+
+    	case TWI_BYTE_ACK_:
+    		if	(++TWI_DATA_POS < TWI_DATA_COUNT){
+    			TWDR = TWI_DATA[TWI_DATA_POS];
+#if TWI_UART_DEBUG==1
+    		sprintf(buf, "%d ", TWDR);
+    		UART_send(buf);
+#endif
+    			TWI_SEND;
+    			break;
+    		}
+#if TWI_UART_DEBUG==1
+    		sprintf(buf, "stoped\r\n");
+    		UART_send(buf);
+#endif
+    		TWI_STOP;
+    		TWI.lastOpOK = 1;
+    		TWI.busy = 0;
+    		break;
+
+    	case TWI_BYTE_NACK_:
+#if TWI_UART_DEBUG==1
+    		sprintf(buf, "failed: BYTE+NACK\r\n");
+    		UART_send(buf);
+#endif
+    		TWI.errorCode = TWSR>>2;
+    		TWI.lastOpOK = 0;
+    		TWI.busy = 0;
+    		TWI_STOP;
+    		break;
+    	}
+    }
+
+}
+
+
---
+++ TWI.h
@@ -0,0 +1,65 @@
+/*
+ * TWI.h
+ *
+ *  Created on: 16 трав. 2016
+ *      Author: alfons-14
+ */
+
+#ifndef TWI_H_
+#define TWI_H_
+
+#define TWI_FREQ 100000
+#define TWI_UART_DEBUG	0 //uses sprintf and very slow
+typedef unsigned char uint8_t;
+
+//actions
+#define TWI_ENABLE 				TWCR = (0<<TWSTA)|(0<<TWSTO)|(0<<TWINT)|(1<<TWEA)|(1<<TWEN)|(1<<TWIE)
+#define TWI_START 				TWCR = (1<<TWSTA)|(0<<TWSTO)|(1<<TWINT)|(1<<TWEA)|(1<<TWEN)|(1<<TWIE)
+#define TWI_STOP 				TWCR = (0<<TWSTA)|(1<<TWSTO)|(1<<TWINT)|(1<<TWEA)|(1<<TWEN)|(1<<TWIE)
+#define TWI_SEND 				TWCR = (0<<TWSTA)|(0<<TWSTO)|(1<<TWINT)|(1<<TWEA)|(1<<TWEN)|(1<<TWIE)
+#define TWI_RECIEVE 			TWCR = (0<<TWSTA)|(0<<TWSTO)|(1<<TWINT)|(1<<TWEA)|(1<<TWEN)|(1<<TWIE)
+#define TWI_RECIEVE_LAST		TWCR = (0<<TWSTA)|(0<<TWSTO)|(1<<TWINT)|(0<<TWEA)|(1<<TWEN)|(1<<TWIE)
+
+//states for master
+#define TWI_BUS_FAIL_			0x00
+#define TWI_START_				0x08
+#define TWI_RESTART_			0x10
+
+//write
+#define TWI_SLA_W_ACK_			0x18
+#define TWI_SLA_W_NACK_			0x20
+#define TWI_BYTE_ACK_			0x28
+#define TWI_BYTE_NACK_			0x30
+
+#define TWI_COLLISION_			0x38
+
+//read
+#define TWI_SLA_R_ACK_			0x40
+#define TWI_SLA_R_NACK_			0x48
+#define TWI_RECIEVE_BYTE_		0x50
+#define TWI_RECIEVE_BYTE_NACK_	0x58
+
+#define TWI_WRITE				0
+#define TWI_READ				1
+
+union TWI_state
+{
+	uint8_t all;
+    struct
+    {
+    	uint8_t busy:1;
+        uint8_t lastOpOK:1;
+        uint8_t errorCode:6;
+
+    };
+};
+
+extern volatile union TWI_state TWI;
+
+void TWI_init();
+void TWI_send(uint8_t, uint8_t*, uint8_t);
+void TWI_send_byte(uint8_t, uint8_t);
+void TWI_receive(uint8_t, uint8_t*, uint8_t);
+void TWI_repeat();
+
+#endif /* TWI_H_ */
